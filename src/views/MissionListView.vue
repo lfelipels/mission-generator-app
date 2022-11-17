2abf17cfce5e39876e16b4c158c985714bbdfa34
lfelipels <felipe.lima@sti.ufc.br>
2022-11-17 15:47:16 -0300

    create mission list page

--- src/views/MissionListView.vue
+++ src/views/MissionListView.vue
@@ -1,3 +1,68 @@
 <template>
-  <div>Missões</div>
+  <div 
+      class="d-flex flex-column align-center justify-content-center mx-auto my-12"
+      style="width: 900px;">
+    <v-row>
+      <v-col cols="12">
+        <v-btn
+          class="ma-2"
+          outlined    
+          @click="clickHandleNewChallenge"
+        >
+          Criar novo desafio
+        </v-btn>
+      </v-col>
+    </v-row>
+        
+    <MissionCard 
+      v-for="(challenge, idx) in challengesByUsers"
+      :key="idx"
+      :challenger-name="challenge.challengerName" 
+      :missions="challenge.missions" 
+      class="mb-2" />
+  </div>
 </template>
+
+<script>
+import { api } from "@/services/api";
+import MissionCard from "../components/MissionCard.vue";
+
+export default {
+  data() {
+    return {
+      challengesByUsers: []
+    }
+  },
+  mounted() {
+    this.loadChallenges();
+  },
+  methods: {
+    async loadChallenges() {
+      this.challengesByUsers = [];
+      try {
+        const resp = await api.get("/challenges");
+
+        for (let name in resp.data) {
+          if (Object.hasOwnProperty.call(resp.data, name)) {
+            this.challengesByUsers.push({
+              challengerName: name,
+              missions: resp.data[name]
+            });
+          }
+        }
+      } catch (error) {
+        //
+        console.log(error);
+      }
+    },
+
+    clickHandleNewChallenge() {
+      this.$router.push({ name: "home"});
+    }
+  },
+  components: {
+    MissionCard
+  }
+}
+</script>
+
